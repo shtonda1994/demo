675ecc0a645d1134185cc0e9ed04d74a40a241aa
shtonda1994 <shtondahonda@gmail.com>
2022-11-20 14:36:00 +0300

    edit home index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("ReactJS");?>
 
-Главная страница
+Главная страница 1
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
